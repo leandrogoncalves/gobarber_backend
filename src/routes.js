3728237948ef4658f6dc67c1da43601dda2f063b
Leandro <leandro.gsilva@fde.sp.gov.br>
2020-04-30 13:57:11 -0300

    Inclusão da entidade provider

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,34 @@
 import { Router } from 'express';
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+
+/**  include  Configs */
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+/** include Middlewares */
 import authMiddleware from './app/middlewares/auth';
 
+/**  include  Controllers */
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
+/** Middleawares */
 routes.use(authMiddleware);
 
+/** User */
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+/** File */
+routes.post('/files', upload.single('file'), FileController.store);
+
+/** Provider */
+routes.get('/providers', ProviderController.index);
 
 export default routes;
